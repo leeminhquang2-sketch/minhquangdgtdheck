512322cda976e4f6807aa7519f38d5940ce5b4a2
leeminhquang2-sketch <leeminhquang2@gmail.com>
2026-06-05 22:14:02 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    admins: [
+        "61567536611221"
+    ],
+    prefix: "!",
+    botName: "SOUL HACK BOT",
+    gameIP: "211.253.26.47",
+    gamePort: "8093",
+    aesKey: "gksekfidjrqjfwk1",
+    aesIV: "towerdefense_amo",
+    servers: {
+        "1": { name: "AMO", display: "AMO (Mobile)" },
+        "2": { name: "ATV", display: "ATV (Android TV)" }
+    },
+    souls: {
+        "1": { id: "SOUL_1", name: "Loki" },
+        "2": { id: "SOUL_2", name: "Gibongi" },
+        "3": { id: "SOUL_3", name: "Hell Knight" },
+        "4": { id: "SOUL_4", name: "Nan-cheon" },
+        "5": { id: "SOUL_5", name: "Dalma" },
+        "6": { id: "SOUL_6", name: "Muk-hyang" },
+        "7": { id: "SOUL_7", name: "Wolf King" },
+        "8": { id: "SOUL_8", name: "Jormungand" }
+    }
+};
